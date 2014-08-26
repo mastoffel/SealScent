c9bfc43ac185c5f035350f6b57f73e828997d1be
Thinkmarti <martin-stoffel@gmx.de>
2014-08-26 22:34:56 +0200

    cleaned scripts

--- scripts/all_scent.R
+++ scripts/all_scent.R
@@ -1,3 +1,4 @@
+all_scent <- function(subgroup, beach) {
 # Main script for analysing scent profiles and associations with genetics
 
 # laoding functions ------------------------------------------------------------
@@ -13,37 +14,37 @@ source("het_results.R")
 #               scent.abundance.pa.csv
 
 scent_abundance <- as.data.frame(t(read.csv(paste("C:\\Users\\Martin\\Studium\\",
-                                "MSc.Behaviour\\Research\\Seal Scent\\R code\\",
-                                "data\\csv_files\\scent abundances.csv", 
-                                sep = ""), row.names=1)))
+                                                  "MSc.Behaviour\\Research\\Seal Scent\\R code\\",
+                                                  "data\\csv_files\\scent abundances.csv", 
+                                                  sep = ""), row.names=1)))
 
 # diversity measures
 scent_diversity <- read.csv(paste("C:\\Users\\Martin\\Studium\\",
                                   "MSc.Behaviour\\Research\\Seal Scent\\",
                                   "R code\\data\\csv_files\\",
                                   "scent diversity.csv", sep = ""),
-                                   row.names=1)
+                            row.names=1)
 
 # relatedness matrix (old: relatedness_41loci.csv)
 relatedness <- read.csv(paste("C:\\Users\\Martin\\Studium\\",
                               "MSc.Behaviour\\Research\\Seal Scent\\",
                               "R code\\data\\csv_files\\",
                               "relatednessnew.csv", sep = ""),
-                               row.names=1)
+                        row.names=1)
 
 ## heterozygosity SH
 heterozygosity <- read.csv(paste("C:\\Users\\Martin\\Studium\\",
                                  "MSc.Behaviour\\Research\\Seal Scent\\",
                                  "R code\\data\\csv_files\\",
                                  "heterozygosity_41loci.csv", sep = ""),
-                                  row.names=1) 
+                           row.names=1) 
 
 # beach and family factor
 factors <- read.csv(paste("C:\\Users\\Martin\\Studium\\",
                           "MSc.Behaviour\\Research\\Seal Scent\\",
                           "R code\\data\\csv_files\\",
                           "factors.csv", sep = ""),
-                           row.names=1) 
+                    row.names=1) 
 
 
 # choose ordination method and get scores---------------------------------------
@@ -53,8 +54,8 @@ allscores <- get_scores(scent_abundance, method = "fa", num_dim = 4,
 
 # get subsets for age ("mums", "pups", "all") and beach (0 = all, 1, 2)---------
 
-allsubsets <- subset_all("mums", beach = 0, scent_abundance,scent_diversity,
-                        relatedness, heterozygosity, allscores, factors)
+allsubsets <- subset_all(subgroup, beach, scent_abundance,scent_diversity,
+                         relatedness, heterozygosity, allscores, factors)
 
 abund <- allsubsets[[1]]
 div <- allsubsets[[2]]
@@ -86,3 +87,13 @@ het_df <- cbind(het_df, scores[, 1:ncol(scores)])
 
 # heterozygosity results
 het_results <- het_results(het_df, num_fac)
+
+out <- list(relate.df=relate_df,
+             results.relatedness=relate_results,
+             relate.list=relate_list,
+             relatedness=relate,
+             het.df=het_df,
+             results.het=het_results,
+             scores=scores,
+             factors=factors)
+}
--- scripts/models.R
+++ scripts/models.R
@@ -7,7 +7,7 @@ scent.abundance <- as.data.frame(t(read.csv(".\\csv_files\\scent abundances.csv"
 
 # Scent Mothership script has to be run
 source("ScentResults.R")
-res <- ScentResults("all","fa",1)
+res <- ScentResults("all","fa", beach = 0)
 
 # subset results list output
 relate.df <- res[[1]]
@@ -56,5 +56,34 @@ names(beachdata)[1] <- "beach"
 
 BeachModel <- ParsModel(beachdata, family="binomial")
 
+# relatedness models wioth ecodist mantel
+source("all_scent.R")
+res <- all_scent("mums", beach=0)
+relate.df.mums <- res[[1]]
 
+source("all_scent.R")
+res <- all_scent("pups", beach=2)
+relate.df.pups <- res[[1]]
 
+res <- ScentResults("mums","fa", beach=1)
+relate.df.mums.1 <- res[[1]]
+
+res <- ScentResults("mums","fa", beach=2)
+relate.df.mums.2 <- res[[1]]
+
+library(ecodist)
+
+mantel_mums <- mantel(relatedness ~ F1 + F2 + F3 + F4, data = relate.df.mums)
+mantel_mums <- mantel(relatedness ~ F2 + F1 + F3 + F4, data = relate.df.mums)
+mantel_mums <- mantel(relatedness ~ F3 + F2 + F3 + F4, data = relate.df.mums)
+mantel_mums <- mantel(relatedness ~ F4 + F1 + F3 + F4, data = relate.df.mums)
+
+mantel_pups <- mantel(relatedness ~ F1 + F2 + F3 + F4, data = relate.df.pups)
+mantel_pups <- mantel(relatedness ~ F2 + F1 + F3 + F4, data = relate.df.pups)
+mantel_pups <- mantel(relatedness ~ F3 + F2 + F3 + F4, data = relate.df.pups)
+mantel_pups <- mantel(relatedness ~ F4 + F1 + F3 + F4, data = relate.df.pups)
+
+detach("package:ecodist", unload=TRUE)
+# taken out
+mantel_mums_B1 <- mantel(relatedness ~ F1 + F2 + F3 + F4, data = relate.df.mums.1)
+mantel_mums_B2 <- mantel(relatedness ~ F1 + F2 + F3 + F4, data = relate.df.mums.2)
